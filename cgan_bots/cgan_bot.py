16f54fc21ad4bed0d1c3eb925280dadc7df91985
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-07-08 17:08:26 +0200

    first working version

--- cgan_bots/cgan_bot.py
+++ cgan_bots/cgan_bot.py
@@ -1,11 +1,54 @@
+from io import BytesIO
 from typing import *
-from telegram.ext import (Updater, MessageHandler, CommandHandler, StringCommandHandler, Filters, Dispatcher)
+
+import numpy as np
+import torch
+import torchvision
+from telegram.ext import (Updater,
+                          CommandHandler,
+                          Dispatcher,
+                          CallbackContext)
+from telegram.update import Update
 
 
 class CGANBot:
-    def __init__(self, bot_key: str):
+    def __init__(self, bot_key: str, model, input_shape: Tuple[int, int, int, int], label_dict: Dict[str, int]):
+        self.label_dict = label_dict
+        self.input_shape = input_shape
+        self.model = model
         self.__updater: Updater = Updater(bot_key, use_context=True)
         self.__dispatcher: Dispatcher = self.__updater.dispatcher
+        self.__dispatcher.add_handler(CommandHandler("classes", self.get_classes))
+        self.__dispatcher.add_handler(CommandHandler("generate", self.gen_image))
+
+    def get_classes(self, update: Update, context: CallbackContext):
+        answer = ", ".join([str(k) for k in self.label_dict.keys()])
+        update.message.reply_text(answer, quote=True)
+
+    def gen_image(self, update: Update, context: CallbackContext):
+        labels = update.message.text.split(" ")
+        if len(labels) < 2:
+            update.message.reply_text("Usage: /generate <class_label>.\nUse /classes to list all supported class labels")
+            return
+        label = labels[1]
+        if label not in self.label_dict.keys():
+            update.message.reply_text("unknown class. use /classes to list all supported class labels")
+            return
+        label = self.label_dict[label]
+        bio = self.generate_image(label)
+
+        update.message.reply_photo(bio, quote=True)
+
+    def generate_image(self, label) -> BytesIO:
+        noise = torch.tensor(np.random.normal(0, 1, self.input_shape), dtype=torch.float)
+        image = self.model(noise, [label])
+        image = (image + 1)/2
+        image = torchvision.transforms.ToPILImage()(image[0])
+        bio = BytesIO()
+        bio.name = 'image.jpeg'
+        image.save(bio, 'JPEG')
+        bio.seek(0)
+        return bio
 
     def run(self):
         """
--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 import logging
 import cgan_bots.cgan_bot
 from secrets import bot_key
+from project_cgan.lib import CDCGAN
 
 if __name__ == '__main__':
+    class_dict = {
+        "bald": 0,
+        "blackhair": 1,
+        "brownhair": 2,
+        "glasses": 3,
+        "mustache": 4,
+    }
+    filters = [1024, 512, 256, 128, 64]
+    input_shape = (1, 100, 1, 1)
+    image_size = 128
+    color_channel = 3
+    model = CDCGAN.load_from_checkpoint(
+        "models/celebA_scheduler_ReduceLROnPlateau_only_g_128_100_1024-512-256-128-64-epoch=29.ckpt",
+        input_dim=input_shape[1],
+        amount_classes=len(class_dict),
+        filter_sizes=filters,
+        color_channels=color_channel,
+        image_size=image_size,
+        device="cpu"
+    )
+    model.eval()
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    b = cgan_bots.cgan_bot.CGANBot(bot_key)
+    b = cgan_bots.cgan_bot.CGANBot(bot_key, model, input_shape, class_dict)
     b.run()
-
